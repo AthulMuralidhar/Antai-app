2bac9c346bf20fd582d24b7009bef0bcb80c9d2d
Athul Muralidhar <athul.m68@gmail.com>
2019-05-22 19:46:40 +0200

    home screen done!

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, 
+  Text, 
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+ } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/temp.png')}
+          style={styles.imgStyle}
+        />
+        <Text style={styles.titleText}>The Antai App</Text>
+        <TouchableOpacity style={styles.btnStyle} onPress={()=>{alert('pressed!')}}>
+          <Text>Play</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+// styling
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+  },
+  imgStyle: {
+    width:200,
+    height:200,
+  },
+  btnStyle:{
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  }
+});
